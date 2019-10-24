cfb698fcb013682b66379d7f6a9857294e3111f6
dominik-guba@web.de <dominik-guba@web.de>
2019-10-24 17:02:52 +0200

    Alibi Sprungfunktion

--- Beispielprojekt/Project Utopia.cpp
+++ Beispielprojekt/Project Utopia.cpp
@@ -28,6 +28,7 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	double x = 100;
 	double y = 500;
+	
 	void update() override
 	{
 		if (input().down(Gosu::KB_D) == true) {
@@ -36,14 +37,29 @@ public:
 		if (input().down(Gosu::KB_A) == true) {
 			x = x - 10;
 		}
+		if (input().down(Gosu::KB_W) == true) {
+			
+			if (y != 200) {
+				y = y - 20;
+			
+			};
+			
+		}
+		
+		if (y < 500.0) {
+				y=y+10;
+		};
 		
 	}
 	void draw() override
 	{
-		bild.draw_rot(x, y, 0.0,	//Position
-			0.0,					//rotation
-			0.5, 1					//Position in relation zu der angegebenen POsition
-		);
+	
+			bild.draw_rot(x, y, 0.0,	//Position
+				0.0,					//rotation
+				0.5, 1					//Position in relation zu der angegebenen POsition
+			); 
+		
+			
 		graphics().draw_quad(
 			0, 500, Gosu::Color::WHITE,
 			800, 500, Gosu::Color::WHITE,

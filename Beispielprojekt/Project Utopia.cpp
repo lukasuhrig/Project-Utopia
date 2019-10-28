40bb525b1bbe7da4aafe5474d3aa1fe59be653fb
Mateusz Koziel <mateusz-koziel@t-online.de>
2019-10-28 21:55:11 +0100

    -bugfixes -background & player class objektorientiert (mehr oder weniger)

--- Beispielprojekt/Project Utopia.cpp
+++ Beispielprojekt/Project Utopia.cpp
@@ -7,73 +7,172 @@
 #include <string>
 #include <iostream>
 
+#include <Gosu/GraphicsBase.hpp>
+
 #include "Vektor2d.h"
 
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
+
+//**********************FUNCTIONS**********************//
+
+
+
+
+//**********************CLASS**********************//
+
+class Player
+{
+	Gosu::Image character;
+
+	double pos_x;
+	double pos_y;
+	bool lookingRight;
+	float health;
+	unsigned score;
+	double jumptime = 0;
+
+public:
+	Player() : character("Held.png")
+	{
+		pos_x = pos_y = score = 0;
+		health = 100.0;
+		lookingRight = true;
+	}
+
+	void turn_left()
+	{
+		lookingRight = false;
+		pos_x = pos_x - 10;
+	}
+	void turn_right()
+	{
+		lookingRight = true;
+		pos_x = pos_x + 10;
+	}
+	void jump()
+	{
+		jumptime = jumptime + (1.0 / 60.0);
+
+		if (pos_y != 200 && pos_y <= 500) {
+			pos_y = 499 + jumptime * jumptime * 1000 - 900 * jumptime;
+		};
+	}
+	void draw() const
+	{
+		character.draw_rot(pos_x, pos_y, 0.0,	//Position
+			0.0,					//rotation
+			0.5, 1					//Position in relation zu der angegebenen POsition
+		);
+	}
+	void set_pos(double x, double y)
+	{
+		pos_x = x;
+		pos_y = y;
+	}
+	double actual_pos_x()
+	{
+		return pos_x;
+	}
+	double actual_pos_y()
+	{
+		return pos_y;
+	}
+	void resetJumpTime()
+	{
+		jumptime = 0;
+	}
+
+};
+
+class Background
+{
+	Gosu::Image background_image;
+	double pos_x;
+	double pos_y;
+
+public:
+	Background() : background_image("Background.png")
+	{
+		pos_x = pos_y = 0;
+	}
+	void move_left()
+	{
+		pos_x = pos_x - 10;
+	}
+	void move_right()
+	{
+		pos_x = pos_x + 10;
+	}
+	void draw() const
+	{
+		background_image.draw_rot(pos_x, pos_y, 0.0,
+			0.0, 0.5, 1);
+	}
+	void set_pos(double x, double y)
+	{
+		pos_x = x;
+		pos_y = y;
+	}
+};
+
 class GameWindow : public Gosu::Window
 {
 public:
-	Gosu::Image bild;
-	GameWindow()
-		: Window(800, 600)
-		,bild("HELD.png")
+		
 
+	Player player;
+	Background background;
+
+	GameWindow() : Window(800, 600)
 	{
 		set_caption("Project Utopia");
+
+		player.set_pos(100,500);
+		background.set_pos(100,200);
 	}
-	// wird bis zu 60x pro Sekunde aufgerufen.
-	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
-	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	double x = 100;
-	double hintergrund_x = 100;
-	double y = 500;
-	double jumptime = 0;
-	void update() override
+
+	void update() override //ca. 60x pro Sekunde
 	{
-		if (input().down(Gosu::KB_D) == true &&x<=700) {
-			x = x + 10;
+		if (input().down(Gosu::KB_D) == true &&player.actual_pos_x()<=(width()-100)) 
+		{
+			player.turn_right();
 		}
-		if (input().down(Gosu::KB_D) == true && x > 700) {
-			hintergrund_x = hintergrund_x - 10;
-		}
-		if (input().down(Gosu::KB_A) == true && x >= 100) {
-			x = x - 10;
-		}
-		if (input().down(Gosu::KB_A) == true && x < 100) {
-			hintergrund_x = hintergrund_x + 10;
-		}
-		if (input().down(Gosu::KB_W) == true||y<499) {
 
-			jumptime=jumptime+(1.0/60.0);
-	
-				if (y != 200&&y<=500) {
-					y = 499+jumptime*jumptime*1000-900*jumptime;
-				};
+		if (input().down(Gosu::KB_D) == true && player.actual_pos_x() > (width() - 100))
+		{
+			background.move_left();
+		}
 
+		if (input().down(Gosu::KB_A) == true && player.actual_pos_x() >= 100) 
+		{
+			player.turn_left();
 		}
-		if (y >= 499) {
-			jumptime = 0;
+
+		if (input().down(Gosu::KB_A) == true && player.actual_pos_x() < 100) 
+		{
+			background.move_right();
 		}
+
+		if (input().down(Gosu::KB_W) == true||player.actual_pos_y()< (height()-101)) 
+		{
+			player.jump();
+		}
+
+		if (player.actual_pos_y() >= (height() - 101))
+		{
+			player.resetJumpTime();
+		}
+
 
 		
 	}
-	void draw() override
+	void draw() override //ca. 60x pro Sekunde
 	{
 	
-			bild.draw_rot(x, y, 0.0,	//Position
-				0.0,					//rotation
-				0.5, 1					//Position in relation zu der angegebenen POsition
-			); 
-			
-		Gosu::Image("Background.png").draw_rot(hintergrund_x, 200, 0.0,
-				0.0,
-				0.5,1
-				
-
-			);
-		
+		player.draw();
+		background.draw();
 			
 		graphics().draw_quad(
 			0, 500, Gosu::Color::WHITE,
@@ -83,10 +182,9 @@ public:
 			0.0
 		);
 	}
-
-	// Wird 60x pro Sekunde aufgerufen
 	
 };
+
 
 // C++ Hauptprogramm
 int main()
@@ -94,3 +192,6 @@ int main()
 	GameWindow window;
 	window.show();
 }
+
+
+//*****************************Funktionen*****************************//
